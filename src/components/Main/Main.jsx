d9250fd2308a70a2c047891c08d983fa4a821b76
ukrwebprom <ukrwebprom@gmail.com>
2023-05-16 11:58:42 +0300

    update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,41 +1,63 @@
-import videoBG from '../../images/clouds.mp4';
-import { ReactComponent as Logo } from '../../images/logo.svg';
-import videoMessage from '../../images/IMG_8332.MP4';
-import { FaPlay } from 'react-icons/fa';
-import { TfiClose } from 'react-icons/tfi';
-import { useState } from 'react';
-import { Modal } from '../Modal/Modal';
-import './main.css';
+import videoBG from "../../images/clouds.mp4";
+import { ReactComponent as Logo } from "../../images/logo.svg";
+import videoMessage from "../../images/IMG_8332.MP4";
+import { FaPlay } from "react-icons/fa";
+import { TfiClose } from "react-icons/tfi";
+import { useState } from "react";
+import { Modal } from "../Modal/Modal";
+import { Oval } from "react-loader-spinner";
+import "./main.css";
 
 export const Main = () => {
-    const [isModal, setIsModal] = useState(false);
+  const [isModal, setIsModal] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    const toggleModal = () => {
-        setIsModal(!isModal);
-    }
+  const toggleModal = () => {
+    setIsModal(!isModal);
+  };
 
-    return (
-        <div className='main'>
-            <video src={videoBG} autoPlay loop muted className='video-back' />
-            <div className='main-content'>
-                <Logo className='logo'/>
-                
-                <div className='main-content-text'>
-                    <button type="button" className='icon-button' onClick={toggleModal}><FaPlay size={20} /></button>
-                    <span className='text-first'>online курс</span>
-                    <span className='text-second'>«ВОЗРОЖДЕНИЕ ГОЛОСА»</span>
-                </div>
-                
-            </div>
-            {isModal && 
-                <Modal>
-                <div className='modal'>
-                    <video src={videoMessage} autoPlay className='video-message'/>
-                    <button type="button" className='close-btn' onClick={toggleModal}><TfiClose size={20} /></button>
-                </div>
-                </Modal>
-                
-            }
+  return (
+    <div className="main">
+      <video src={videoBG} autoPlay loop muted className="video-back" />
+      <div className="main-content">
+        <Logo className="logo" />
+
+        <div className="main-content-text">
+          <button type="button" className="icon-button" onClick={toggleModal}>
+            <FaPlay size={20} />
+          </button>
+          <span className="text-first">online курс</span>
+          <span className="text-second">«ВОЗРОЖДЕНИЕ ГОЛОСА»</span>
         </div>
-    )
-}
+      </div>
+      {isModal && (
+        <Modal>
+          <div className="modal">
+              <Oval
+                height={80}
+                width={80}
+                color="#C478FF"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={!isLoaded}
+                ariaLabel="oval-loading"
+                secondaryColor="#EFDBFE"
+                strokeWidth={1}
+                strokeWidthSecondary={1}
+              />
+            <video
+              src={videoMessage}
+              autoPlay
+              className="video-message"
+              onLoadedData={() => setIsLoaded(true)}
+              onEnded={toggleModal}
+            />
+            <button type="button" className="close-btn" onClick={toggleModal}>
+              <TfiClose size={20} />
+            </button>
+          </div>
+        </Modal>
+      )}
+    </div>
+  );
+};
