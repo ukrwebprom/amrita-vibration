a014762f2c46e7b8ce239ee43e0c768fe853ab87
ukrwebprom <ukrwebprom@gmail.com>
2023-05-18 00:34:23 +0300

    text blocks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './images/logo.svg';
 import './App.css';
 import { Main } from './components/Main/Main';
 import { ContactButtons } from './components/ContactButtons/ContactButtons';
+import { TextBlockOne } from './components/TextBlocks/TextBlockOne';
 
 function App() {
   return (
     <div className="App">
     <Main />
     <ContactButtons />
+    <TextBlockOne />
     </div>
   );
 }
---
+++ src/components/TextBlocks/TextBlockOne.jsx
@@ -0,0 +1,57 @@
+import "./textblock.scss";
+import { CgSandClock } from "react-icons/cg";
+
+export const TextBlockOne = () => {
+  return (
+    <div className="container">
+    <div className="text-blocks">
+      <div>
+        <div className="textblock-part">
+          <p>Часть 1</p>
+          <div className="textblock-duration">
+            <CgSandClock />
+            <p>Длительность 1 мес.</p>
+          </div>
+        </div>
+        <h2 className="textblock-title">Возрождение Голоса</h2>
+
+        <ul className="textblock-list">
+          <li>Вступление лекция</li>
+          <li>Сила потокового пения /лекция</li>
+          <li>Медитация через пение & Совместное звучание</li>
+          <li>Запуск тела и дыхания для звучания</li>
+          <li>Энергетическое пение</li>
+          <li>Мантра-медитация - коды</li>
+          <li>Древние чаши. Крийя-активация энергии</li>
+          <li>Освобождение тела. Открытие звучания телом</li>
+          <li>Активация живота и груди. Потоковое пение</li>
+          <li>Крии Голоса</li>
+        </ul>
+      </div>
+      <div>
+        <div className="textblock-part">
+          <p>Часть 2</p>
+          <div className="textblock-duration">
+            <CgSandClock />
+            <p>Длительность 1 мес.</p>
+          </div>
+        </div>
+        <h2 className="textblock-title">СВОБОДА ГОЛОСА</h2>
+
+        <ul className="textblock-list">
+          <li>Вокальный диапазон</li>
+          <li>Технология ритма</li>
+          <li>Сила Голоса</li>
+          <li>Основа импровизации</li>
+          <li>Эмоциональное пение/сексуальная энергия</li>
+          <li>Медитация одного звука</li>
+          <li>Распевки для укрепления голоса</li>
+          <li>Импровизация с музыкальными инструментами</li>
+          <li>Распаковка своей уникальности</li>
+          <li>Прямой эфир / вопросы-ответы</li>
+        </ul>
+      </div>
+      </div>
+    </div>
+  );
+};
